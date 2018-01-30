466a366ba778874d12991f51f27d5b7c9432df47
Matty <matty.taylor0@gmail.com>
2018-01-30 14:19:19 +0000

    profile page and form php

--- clientprofilePage.php
+++ clientprofilePage.php
@@ -13,15 +13,25 @@ echo makeHeader();
     <p class="profilepagelocation">Newcastle upon Tyne</p>
 
     <div class="form-container-profile">
-      <input type="submit" value="Notifications" class="button">
-      <input type="submit" value="Edit Profile" class="button">
-      <input type="submit" value="Messages" class="button">
+      <a href="notifications.php" class="button">Notifications</a>
+      <a href= "editProfile.php" class="button">Edit Profile</a>
+      <a href= "messaging.php" class="button">Messages</a>
     </div>
   </div>
 </div>
 
 <h3 id="activejobtitle">My Active Job Posts</h3>
 
-<?php 
+<p id="rcorners1">Newcastle Public House Build</p>
+<p id="rcorners2">EDIT DELETE</p>
+
+<p id="rcorners1">Landscape Request</p>
+<p id="rcorners2">EDIT DELETE</p>
+
+<br>
+<br>
+<br>
+
+<?php
 echo makePageFooter();
 ?>
