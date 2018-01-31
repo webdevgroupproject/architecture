39a4c1719f14d865f4f5b7e59c6f33ed86163363
Ross Brown <ross3.brown@northumbria.ac.uk>
2018-01-31 13:44:31 +0000

    pagination on events and forum

--- scripts/functions.php
+++ scripts/functions.php
@@ -116,7 +116,7 @@ function startSession(){
 
     //ini_set("session.save_path", "/Applications/MAMP/sessionData");
 
-    ini_set("session.save_path", "/xampp/sessionData");
+    ini_set("session.save_path", "/xampp1/sessionData");
 
     session_start();
 };
