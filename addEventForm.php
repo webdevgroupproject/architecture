4b0fc75fe1987e1f3d19f9ec460747cd1865b22b
Ross Brown <ross3.brown@northumbria.ac.uk>
2018-02-07 15:23:14 +0000

    form styling

--- addEventForm.php
+++ addEventForm.php
@@ -17,31 +17,28 @@ echo makeHeader();
                 <label>Address line 2 (optional): </label>
                 <input type="text" name="address2">
                 <label>City: </label>
-                <input type="text" name="city" class="form-control block" placeholder="Please enter the first line of the address"
+                <input type="text" name="city" class="form-control block" placeholder="Please enter the city"
                        data-validation="required">
                 <label>Postcode: </label><br>
-                <input style="width: 150px;" type="text" name="postcode" class="form-control block" placeholder="Please enter the first line of the address"
+                <input style="width: 150px;" type="text" name="postcode" class="form-control block" placeholder="Please enter a valid UK postcode"
                        data-validation="required"><br>
             </fieldset>
             <fieldset>
                 <legend>When</legend>
                 <label>Date: </label><br>
-                <input style="width: 150px;" type="date" name="date" class="form-control block" placeholder="Please enter the first line of the address"
-                       data-validation="required" data-validation-help="Please enter a username"><br>
+                <input style="width: 150px;" type="date" name="date" class="form-control block" data-validation="required" data-validation-help="Please enter a username"><br>
                 <label>Time: </label><br>
-                <input style="width: 150px;" type="time" name="time" class="form-control block" placeholder="Please enter the first line of the address"
-                       data-validation="required"><br>
+                <input style="width: 150px;" type="time" name="time" class="form-control block" data-validation="required"><br>
             </fieldset>
             <fieldset>
                 <legend>Additional info</legend>
                 <label>Event name: </label>
-                <input type="text" name="name" class="form-control block" placeholder="Please enter the first line of the address"
+                <input type="text" name="name" class="form-control block" placeholder="Please enter the event name"
                        data-validation="required">
                 <label>Spaces: </label><br>
-                <input style="width: 150px;" type="number" name="spaces" class="form-control block" placeholder="Please enter the first line of the address"
-                       data-validation="required"><br>
+                <input style="width: 150px;" type="number" name="spaces" class="form-control block" data-validation="required"><br>
                 <label>Event Information: </label>
-                <textarea style="max-width: 100%; width: 100%; margin-bottom: 1em;" name="info" id='info' class="form-control block" placeholder="Please enter the first line of the address"
+                <textarea style="max-width: 100%; width: 100%; margin-bottom: 1em;" name="info" id='info' class="form-control block" placeholder="Please enter some information about the event"
                           data-validation="required"></textarea>
                 <label>Image: </label>
                 <input type="file" name="image">
--- scripts/functions.php
+++ scripts/functions.php
@@ -117,10 +117,10 @@ FOOTER;
 
 function startSession(){
 
-    ini_set("session.save_path", "/Applications/MAMP/sessionData");
+ //   ini_set("session.save_path", "/Applications/MAMP/sessionData");
 
 
-//    ini_set("session.save_path", "/xampp1/sessionData");
+    ini_set("session.save_path", "/xampp1/sessionData");
 
 
 
