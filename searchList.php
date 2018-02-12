03eeee90c8065d5dac345b57b74ecd75d0127e26
matthew.barker <m.barker@northumbria.ac.uk>
2018-02-12 22:00:55 +0000

    Search style & function

--- searchList.php
+++ searchList.php
@@ -8,6 +8,7 @@ echo makeHeader();
 $search = filter_has_var(INPUT_GET, 'searchInput') ? $_GET['searchInput'] : null;
 $searchChoice = filter_has_var(INPUT_GET, 'searchChoice') ? $_GET['searchChoice'] : null;
 $search = filter_var($search, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
+$filter = filter_has_var(INPUT_GET, 'filter') ? $_GET['filter'] : null;
 
 $dbConn = databaseConn::getConnection();
 $userType = checkUserType();
@@ -17,16 +18,104 @@ if (isset($searchChoice)){
     if (empty($search)) {
       $searchSQL = "SELECT *
               FROM bp_user
-              WHERE userRole = '$searchChoice'";
+              WHERE userRole = '$searchChoice'
+              ORDER BY '$filter'";
     } else {
       $searchSQL = "SELECT *
                FROM bp_user
-               WHERE forname LIKE '%$search%'
+               WHERE forename LIKE '%$search%'
                OR surname LIKE '%$search%'
                OR email LIKE '%$search%'
                AND userRole = '$searchChoice'
-               ORDER BY forname ASC";
+               ORDER BY '$filter'";
     }
+
+    echo "
+      <h1>Search results</h1>
+      <div class='searchFilter'>
+        <form method='get' action='searchList.php'>
+          <legend>Filters</legend>
+          <span>
+            <input type='radio' name='filter' value='forename ASC' checked/>
+            <label for='filterA-Z'>Forename - A to Z</label>
+          </span>
+          <span>
+            <input type='radio' name='filter' value='surname ASC'/>
+            <label for='filter'>Surname - A to Z</label>
+          </span>
+          <span>
+            <input type='radio' id='filterRole' name='filter' value='forename ASC'/>
+            <label for='filterRole'>By role</label>
+          </span>
+          <span>
+            <input type='radio' id='filterSkill' name='filter' value='forename ASC'/>
+            <label for='filterSkill'>By skill</label>
+          </span>
+          <input type='text' name='searchInput' style='display:none;' value='$search'/>
+          <input type='text' name='searchChoice' style='display:none;' value='$searchChoice'/>
+          <input type='submit' class='button'>
+        </form>
+      </div>
+    ";
+
+    if ($searchstmt = $dbConn->query($searchSQL)) {
+     $srows = $searchstmt->fetchAll(PDO::FETCH_OBJ);
+     $snum_rows = count($srows);
+     echo "
+       <div class='searchResults'>
+     ";
+
+     if ($snum_rows > 0) {
+       foreach ($srows as $listing) {
+         $id = $listing->userId;
+         $forename = $listing->forename;
+         $surname = $listing->surname;
+         $email = $listing->email;
+         $profilePic = $listing->image;
+         $org = $listing->organisation;
+         $local = $listing->location;
+         $oView = $listing->overview;
+
+         if ($listing->pro == 1) {
+           $pro = "star";
+         } else {
+           $pro = "";
+         }
+
+         echo "
+            <div class='flSearch'>
+              <img src=$profilePic/>
+              <div class='listing-body'>
+                <span class='heading'>
+                  <h2>$forename $surname</h2>
+                  <i class=\"material-icons\">$pro</i>
+                </span>
+                <p>Email: $email</p>
+                <p>Organisation: $org</p>
+                <p>Location: $local</p>
+                <p>Description: $oView</p>
+              </div>
+              <div class='listing-buttons'>
+                <form method='get' action='profile.php?userID=$id'>
+                  <input type='submit' class='button' href='profile.php' value='View'/>
+                </form>
+                <form method='get' style='float: right !important;' action='messaging.php?userID=$id'>
+                  <input type='submit' class='button' value='Message'/>
+                </form>
+              </div>
+           </div>
+         ";
+       }
+     }
+     echo "
+       </div>
+     ";
+    }
+  } elseif ($searchChoice == 'jobs') {
+    $searchSQL = "select *
+             from bp_job_post
+             WHERE jobName LIKE '%$search%'
+             order by jobName ASC";
 
     echo "
       <h1>Search results</h1>
@@ -63,50 +152,6 @@ if (isset($searchChoice)){
 
      if ($snum_rows > 0) {
        foreach ($srows as $listing) {
-         $forename = $listing->forename;
-         $surname = $listing->surname;
-         $email = $listing->email;
-         $profilePic = $listing->image;
-
-         if ($listing->pro == 1) {
-           $pro = "star";
-         } else {
-           $pro = "";
-         }
-
-         echo "
-           <div class='flSearch'>
-             <img src=$profilePic/>
-             <span class='heading'>
-                <h2>$forename $surname</h2>
-                <i class=\"material-icons\">$pro</i>
-              </span>
-             <p>Email: $email</p>
-           </div>
-         ";
-       }
-     }
-     echo "
-       </div>
-     ";
-    }
-  } elseif ($searchChoice == 'jobs') {
-    $searchSQL = "select *
-             from bp_job_post
-             WHERE jobName LIKE '%$search%'
-             order by jobName ASC";
-
-    echo "<h1>Search results</h1>";
-
-    if ($searchstmt = $dbConn->query($searchSQL)) {
-     $srows = $searchstmt->fetchAll(PDO::FETCH_OBJ);
-     $snum_rows = count($srows);
-     echo "
-       <div class='searchResults'>
-     ";
-
-     if ($snum_rows > 0) {
-       foreach ($srows as $listing) {
          $name = $listing->jobName;
          $desc = $listing->jobDesc;
          $location = $listing->jobLoc;
@@ -118,9 +163,9 @@ if (isset($searchChoice)){
            <div class='jSearch'>
              <img src=''/>
              <h2>$name</h2>
-             <p>$desc</p>
-             <p>$location</p>
-             <p>$duration</p>
+             <p>Job description: $desc</p>
+             <p>Location: $location</p>
+             <p>Duration: $duration</p>
            </div>
          ";
        }
